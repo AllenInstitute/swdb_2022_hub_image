19ac51c4ec8e7cc5db549c7f0ea9684d5ecb70ba
Luke Campagnola <luke.campagnola@gmail.com>
2022-08-11 17:16:11 -0700

    add man, bash-completion packages, and script for assembling all notebooks for testing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -l
 
+cd /home/jovyan
 touch ~/test.file
 
-git clone https://github.com/AllenInstitute/swdb_2022
+git clone https://$JUPYTERHUB_USER@github.com/AllenInstitute/swdb_2022 swdb_2022_test
+cd swdb_2022_test
+git merge origin/initial_EM
+git clone https://$JUPYTERHUB_USER@github.com/AllenInstitute/swdb_2022_python_bootcamp
+mv swdb_2022_python_bootcamp/Python* .
+mv swdb_2022_python_bootcamp/support_files .
+rm -rf swdb_2022_python_bootcamp
+git clone https://$JUPYTERHUB_USER@github.com/corbennett/swdb_2022 swdb_2022_corbennett
+cp -r swdb_2022_corbennett/Organizers/* DynamicBrain/
+rm -rf swdb_2022_corbennett
+
